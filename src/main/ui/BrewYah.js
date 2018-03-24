18a6fa37ccdfa651497658f9d202c80cdb899032
Josh Cox <cox.josh.d@gmail.com>
2018-03-24 17:10:10 -0500

    New: BrewYah UI Component

---
+++ src/main/ui/BrewYah.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class BrewYah extends React.Component {
+    render(props) {
+        return (<h1>Hello World 2</h1>)
+    }
+}
+
+ReactDOM.render(<BrewYah/>, document.getElementById('brewyah'));
